d8a7751d5e71bbce5135e8cb6106e011772809f1
Oussama BONNOR <oussamabonnor@gmail.com>
2017-08-16 18:33:14 +0100

    deleting unnecessary

--- src/com/company/LoginPanel.java
+++ src/com/company/LoginPanel.java
@@ -4,12 +4,8 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import java.awt.Color;
-import java.awt.GridBagLayout;
 import javax.swing.JTextField;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import java.awt.Toolkit;
 
 import javax.swing.JPasswordField;
